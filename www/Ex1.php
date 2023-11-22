bc58a5502a30f345fe1fc7fffb60ae4e2b4ed144
DovahkiinH <loick.caplier@gmail.com>
2023-11-22 15:19:42 +0100

    PHP

---
+++ www/Ex1.php
@@ -0,0 +1,13 @@
+<html>
+
+<body>
+
+    <?php 
+    echo "l'adresse IP du serveur est :" , $_SERVER["SERVER_ADDR"];
+
+    echo "l'adresse IP du client est :" , $_SERVER["REMOTE_ADDR"];
+    ?>
+    
+</body>
+
+</html>
---
+++ www/Ex2(1).php
@@ -0,0 +1,13 @@
+<html>
+
+<body>
+
+    <?php 
+    for ($i = 1; $i <= 150; $i += 2) {
+        echo $i . " ";
+    }
+    ?>
+    
+</body>
+
+</html>
