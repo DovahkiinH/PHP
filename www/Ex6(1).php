781798c835a59490736c8930fbc138c1a7808560
DovahkiinH <loick.caplier@gmail.com>
2023-11-28 14:57:52 +0100

    PHP

--- www/Ex6(1).php
+++ www/Ex6(1).php
@@ -2,14 +2,35 @@
 
 <body>
 
+Lecture d'un fichier
 
+Téléchargez ce fichier, il contient une liste de sites indispensables à la compréhension du monde moderne.
 
+Écrire un programme qui lit ce fichier et qui construit une page web contenant une liste de liens hypertextes.
 
-    <?php
-        
+Utilisez la fonction file() qui permet de lire directement un fichier et renvoie son contenu sous forme de tableau.
 
-        
-    ?>
+<?php
+// Nom du fichier à lire
+$nomFichier = 'liens.txt';
+
+// Lire le contenu du fichier dans un tableau
+$contenuFichier = file($nomFichier, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+// Vérifier si le fichier a pu être lu
+if ($contenuFichier !== false) {
+    echo '<ul>';
+
+    // Parcourir le tableau et afficher chaque ligne comme un lien
+    foreach ($contenuFichier as $ligne) {
+        echo '<li><a href="' . $ligne . '">' . $ligne . '</a></li>';
+    }
+
+    echo '</ul>';
+} else {
+    echo 'Erreur lors de la lecture du fichier.';
+}
+?>
     
 </table>
 
---
+++ www/Ex6(2).php
@@ -0,0 +1,25 @@
+<html>
+
+<body>
+
+Un site partenaire met à votre disposition une liste des nouveaux utilisateurs inscrits.
+
+Cette liste est disponible à cette adresse https://ncode.amorce.org/customers.csv.
+
+Il s'agit d'un fichier CSV où chaque ligne représente un nouvel utilisateur. Les différents champs sont Surname, Firstname, Email, Phone, City, State, ils sont séparés par une virgule ,.
+
+Utilisez la fonction file() pour récupérer le contenu de ce fichier.
+
+Découpez chaque ligne en utilisant une des fonctions suivantes: explode() ou preg_split()
+
+Affichez le résultat dans un tableau HTML avec Bootstrap en prenant bien soin de nommer les colonnes du tableau.
+
+<?php
+
+
+
+?>
+
+</body>
+
+</html>
